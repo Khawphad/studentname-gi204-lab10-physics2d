0680aed75f6673c727340f64a2c29f13248df168
Khawphad <danaiphat.joik@bumail.net>
2025-04-24 17:09:49 +0700

    player move and walk and jump

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    float move;
+    [SerializeField]float jumpForce;
+    [SerializeField] bool Isjumping;
+
+    Rigidbody2D rb2d;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+
+        if(Input.GetButtonDown("Jump")&& !Isjumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("jump");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Isjumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Isjumping = true;
+        }
+    }
+}
